1f7e897deba42c5c6bc55372665cb32494b42496
Dudododo <a752601260@gmail.com>
2020-04-17 15:14:02 +0800

    .

--- findContours1.cpp
+++ findContours1.cpp
@@ -7,15 +7,22 @@ using namespace cv;
 int main()
 {
 	cv::Mat srcMat = imread("rim.png", 1);
-	cv::Mat binMat;
+	cv::Mat binMat, dstMat;
 
-	//大津法
-	threshold(srcMat, binMat, 125, 255, THRESH_BINARY);
-
+	cvtColor(srcMat, dstMat, COLOR_BGR2GRAY);
+	threshold(dstMat, binMat, 140, 255, THRESH_BINARY);
+	
 	vector<vector<Point>> contours;
 	vector<Vec4i> hierarchy;
-	findContours(binMat, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
-
+	cv::findContours(binMat, contours, RETR_LIST, CHAIN_APPROX_NONE);
+	//绘制轮廓
+	for (int i = 0; i < contours.size(); i++)
+	{
+		RotatedRect rbox = minAreaRect(contours[i]);
+		if (fabs(rbox.size.width  / rbox.size.height - 1) < 0.1 && rbox.size.width > 15 && rbox.size.height > 15)
+			drawContours(srcMat, contours, i, Scalar(0, 255, 255), -1, 8);
+	}
+	
 	imshow("srcMat", srcMat);
 	imshow("binMat", binMat);
 	waitKey(0);
